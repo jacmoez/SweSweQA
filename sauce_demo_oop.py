8a2891b29b1945b6f8375bc58e4927643f0b3c74
Zay Yar Phyo <streammineking@gmail.com>
2024-11-27 22:51:14 +0630

    Txt Report Nov 27

--- sauce_demo_oop.py
+++ sauce_demo_oop.py
@@ -4,54 +4,59 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
+def report_txt(test="",msg="",file_name="sauce_demo_report.txt"):
+    with open(file_name,"a") as f:
+        if test:
+            f.write(f"{test}\nMessage: {msg}\n{"="*40}\n")
+        else:
+            f.write(f"{msg}\n")
+
 class SwagTst:
+    driver = webdriver.Firefox()
 
-    def __init__(self):
-        self.driver = webdriver.Firefox()
-
-    def open_browser(self):
+    def test_open_browser(self):
         self.driver.get("https://www.saucedemo.com/")
-        print("Test 1 : Open browser success!")
+        report_txt("Test 1 : Open browser success!")
 
-    def login_test(self):
+    def test_login_test(self):
         self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
         time.sleep(2)
         self.driver.find_element(By.XPATH, "//*[@id='password']").send_keys("secret_sauce", Keys.RETURN)
-        print("Test 2 : Login Success!")
+        report_txt("Test 2 : Login Success!")
 
-    def check_page_go(self):
+    def test_check_page_go(self):
         expected_url = "https://www.saucedemo.com/inventory.html"
         actual_url = self.driver.current_url
 
         assert expected_url == actual_url
         assert "Swag Labs" in self.driver.title
-        print("Test 3 : verified success")
+        report_txt("Test 3 : verified success", "Ok!")
 
-    def add_to_cart(self):
+    def test_add_to_cart(self):
         self.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
         time.sleep(2)
         self.driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
         time.sleep(2)
         self.driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
-        print("Test 4 : Add item to Cart")
+        report_txt("Test 4 : Add item to Cart")
 
-    def remove_item(self):
+    def test_remove_item(self):
         self.driver.find_element(By.ID, "remove-sauce-labs-fleece-jacket").click()
-        print("Test 5 : Remove 1 item")
+        report_txt("Test 5 : Remove 1 item")
 
-    def view_order_items(self):
+    def test_view_order_items(self):
         self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-        print("Test 6 : View Order Item")
+        report_txt("Test 6 : View Order Item")
 
-    def check_cart_page_go(self):
+    def test_check_cart_page_go(self):
         expected_url = "https://www.saucedemo.com/cart.html"
         actual_url = self.driver.current_url
 
         assert expected_url == actual_url
         assert "Swag Labs" in self.driver.title
-        print("Test 7 : check cart page success")
+        report_txt("Test 7 : check cart page success")
 
-    def view_cart_ditail(self):
+    def test_view_cart_ditail(self):
         self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/div/div[1]/div[3]/div[2]/a/div").click()
 
         self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
@@ -59,9 +64,9 @@ class SwagTst:
         self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/div/div[1]/div[4]/div[2]/a/div").click()
 
         self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-        print("Test 8 : view cart ditail Success!")
+        report_txt("Test 8 : view cart ditail Success!")
 
-    def check_out(self):
+    def test_check_out(self):
         self.driver.find_element(By.ID, "checkout").click()
         self.driver.find_element(By.ID, "first-name").send_keys("QA")
         time.sleep(2)
@@ -70,36 +75,37 @@ class SwagTst:
         self.driver.find_element(By.ID, "postal-code").send_keys("11111")
         time.sleep(2)
         self.driver.find_element(By.ID, "continue").click()
-        print("Test 9 : Check Out Success!")
+        report_txt("Test 9 : Check Out Success!")
 
-    def check_checkout(self):
+    def test_check_checkout(self):
         item_total = self.driver.find_element(By.CLASS_NAME, "summary_subtotal_label").text
         tax = self.driver.find_element(By.CLASS_NAME, "summary_tax_label").text
         total = self.driver.find_element(By.CLASS_NAME, "summary_total_label").text
-        print(f"{item_total}.")
-        print(f"{tax}.")
-        print(f"{total}")
-        print("="*30)
+        report_txt(f"{item_total}.")
+        report_txt(f"{tax}.")
+        report_txt(f"{total}")
+        #report_txt("="*30)
 
     def finish(self):
         self.driver.find_element(By.ID, "finish").click()
         complete = self.driver.find_element(By.CLASS_NAME, "complete-header").text
-        print(complete)
+        time.sleep(2)
+        report_txt(complete)
         self.driver.find_element(By.ID, "back-to-products").click()
-        print('Test 10 : Finish Success')
-        print("="*50)
+        report_txt('Test 10 : Finish Success')
+        #report_txt("="*50)
 
     def main(self):
-        self.open_browser()
-        self.login_test()
-        self.check_page_go()
-        self.add_to_cart()
-        self.remove_item()
-        self.view_order_items()
-        self.check_cart_page_go()
-        self.view_cart_ditail()
-        self.check_out()
-        self.check_checkout()
+        self.test_open_browser()
+        self.test_login_test()
+        self.test_check_page_go()
+        self.test_add_to_cart()
+        self.test_remove_item()
+        self.test_view_order_items()
+        self.test_check_cart_page_go()
+        self.test_view_cart_ditail()
+        self.test_check_out()
+        self.test_check_checkout()
         self.finish()
 
 
