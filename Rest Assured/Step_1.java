0111a3e950b0ff0b6662d9cfa28a6ea90bd9504b
Zay Yar Phyo <streammineking@gmail.com>
2025-05-21 23:47:53 +0630

    Update Step_1.java

--- Rest Assured/Step_1.java
+++ Rest Assured/Step_1.java
@@ -1,56 +1,86 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class Step_1 {
 
     @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
+    public static void setup(){
+      RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
     }
 
     @Test
     @Order(1)
-    void testGetAllPost() {
-        ((ValidatableResponse)((ValidatableResponse)((Response)RestAssured.given().when().get("/posts", new Object[0])).then()).statusCode(200)).body("$.size()", Matchers.greaterThan(0), new Object[0]);
+     void testGetAllPost() {
+        given().when().get("/posts").then().statusCode(200).body("$.size()", greaterThan(0));
     }
     @Test
     @Order(2)
-    public void testGetPostById() {
-        ((ValidatableResponse)((ValidatableResponse)((Response)RestAssured.given().pathParams("id", 1, new Object[0]).when().get("/posts/{id}", new Object[0])).then()).statusCode(200)).body("id", Matchers.equalTo(1), new Object[0]);
+    public void testGetPostById(){
+        given()
+                .pathParams("id",1)
+                .when().get("/posts/{id}")
+                .then().statusCode(200)
+                .body("id", equalTo(1));
     }
     @Test
     @Order(3)
     public void testCreatePost() {
-        ((ValidatableResponse)((ValidatableResponse)((Response)RestAssured.given().contentType("application/json").body("{ \"title\":  \"New Post\", \"content\":  \"This is a test post\"}").when().post("/posts", new Object[0])).then()).statusCode(201)).body("title", Matchers.equalTo("New Post"), new Object[0]);
+        given()
+                .contentType("application/json")
+                .body("{ \"title\":  \"New Post\", \"content\":  \"This is a test post\"}")
+                .when()
+                .post("/posts")
+                .then()
+                .statusCode(201)
+                .body("title", equalTo("New Post"));
     }
     @Test
-    @Order(4)
-    public void testDeletePost() {
-        ((ValidatableResponse)((Response)RestAssured.given().pathParams("id", 1, new Object[0]).when().delete("/posts/{id}", new Object[0])).then()).statusCode(Matchers.anyOf(Matchers.is(200), Matchers.is(202), Matchers.is(204), Matchers.is(404)));
-    }
-
-    @Order(5)
-    public void testUpdatePost() {
-        ((ValidatableResponse)((ValidatableResponse)((Response)RestAssured.given().contentType("application/json").pathParams("id", 1, new Object[0]).body("{\"title\":  \"Update Post\", \"content\":  \"Update content\"}").when().put("/posts/{id}", new Object[0])).then()).statusCode(200)).body("title", Matchers.equalTo("Update Post"), new Object[0]);
-    }
+     @Order(4)
+       public void testDeletePost() {
+        given()
+                .pathParams("id", 1)
+                .when()
+                .delete("/posts/{id}")
+                .then()
+                .statusCode(anyOf(is(200), is(202), is(204), is(404)));
+       }
+       @Test
+       @Order(5)
+       public void testUpdatePost(){
+        given()
+                .contentType("application/json")
+                .pathParams("id", 1)
+                .body("{\"title\":  \"Update Post\", \"content\":  \"Update content\"}")
+                .when()
+                .put("/posts/{id}")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo("Update Post"));
+       }
     @Test
     @Order(6)
-    public void testCommentsForPost() {
-        ((ValidatableResponse)((ValidatableResponse)((Response)RestAssured.given().when().get("/posts/1/comments", new Object[0])).then()).statusCode(200)).body("$.size()", Matchers.greaterThan(0), new Object[0]);
+    public void testCommentsForPost(){
+        given()
+                .when()
+                .get("/posts/1/comments")
+                .then()
+                .statusCode(200)
+                .body("$.size()", greaterThan(0));
     }
     @Test
     @Order(7)
-    public void testGetCommentsPostsIdQueryParam() {
-        ((ValidatableResponse)((ValidatableResponse)((Response)RestAssured.given().queryParam("postId", new Object[]{1}).when().get("/comments", new Object[0])).then()).statusCode(200)).body("[0].postId", Matchers.equalTo(1), new Object[0]);
+    public void testGetCommentsPostsIdQueryParam(){
+        given()
+                .queryParam("postId", 1)
+                .when()
+                .get("/comments")
+                .then()
+                .statusCode(200)
+                .body("[0].postId", equalTo(1));
     }
 }
